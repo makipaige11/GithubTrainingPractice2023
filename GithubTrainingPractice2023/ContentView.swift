3bcdf089f980fe33565469d840b32a6e89e7ecf2
Paige <makipaige11@gmail.com>
2023-04-18 23:41:41 -0500

    Initial Commit

---
+++ GithubTrainingPractice2023/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GithubTrainingPractice2023
+//
+//  Created by Paige Maki on 4/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("FinnickChristmas")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(5)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubTrainingPractice2023/GithubTrainingPractice2023App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTrainingPractice2023App.swift
+//  GithubTrainingPractice2023
+//
+//  Created by Paige Maki on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubTrainingPractice2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
